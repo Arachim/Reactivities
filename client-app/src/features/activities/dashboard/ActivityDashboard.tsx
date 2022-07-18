2501fc042df89ef2b3928eb2deded180414e6008
נועה לנצנר <noa_l@malam.com>
2022-07-18 12:40:12 +0300

    end of section 5

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, List } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
@@ -7,18 +7,31 @@ import ActivityList from "./ActivityList";
 
 interface Props {
     activities: Activity[];
+    selected: Activity | undefined;
+    select: (id: string) => void;
+    cancelSelect: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    delete_: (id: string) => void;
 }
 
-export default function ActivityDashboard({activities} : Props) {
+export default function ActivityDashboard({activities, selected,
+        select, cancelSelect, editMode, openForm, closeForm, createOrEdit, delete_} : Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} />
+                <ActivityList activities={activities} selectActivity={select} delete_={delete_} />
             </Grid.Column>
             <Grid.Column width='6'>
-                {activities[0] &&
-                <ActivityDetails activity={activities[0]} />}
-                <ActivityForm />
+                {selected && !editMode &&
+                <ActivityDetails
+                    activity={selected}
+                    cancelSelectActivity={cancelSelect}
+                    openForm={openForm} />}
+                    {editMode &&
+                <ActivityForm closeForm={closeForm} activity={selected} createOrEdit={createOrEdit} />}
             </Grid.Column>
         </Grid>
     )
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Button, Grid, Item, ItemDescription, Label, List, Segment } from "semantic-ui-react";
+import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
 interface Props {
     activities: Activity[];
+    selectActivity: (id: string) => void;
+    delete_: (id: string) => void;
 }
 
-export default function ActivityList({activities}: Props) {
+export default function ActivityList({activities, selectActivity, delete_}: Props) {
     return (
         <Segment>
             <Item.Group divided>
@@ -20,7 +22,8 @@ export default function ActivityList({activities}: Props) {
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button floated="right" content='View' color="blue" />
+                                <Button onClick={() => selectActivity(activity.id)} floated="right" content='צפה' color="blue" />
+                                <Button onClick={() => delete_(activity.id)} floated="right" content='מחק' color='red' />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
